39e60041b7700081a5b074f9c8f82e89f406e556
Nhan3010 <87415388+Nhan3010@users.noreply.github.com>
2021-07-14 15:55:36 +0700

    Update wsgi.py

--- pythonweb/wsgi.py
+++ pythonweb/wsgi.py
@@ -1,16 +1,12 @@
-"""
-WSGI config for pythonweb project.
-
-It exposes the WSGI callable as a module-level variable named ``application``.
-
-For more information on this file, see
-https://docs.djangoproject.com/en/3.2/howto/deployment/wsgi/
-"""
-
 import os
-
+import sys
+ 
+path = '/home/<PythonAnywhere-username>/phocode-site'
+if path not in sys.path:
+ sys.path.append(path)
+ 
+os.environ['DJANGO_SETTINGS_MODULE'] = 'PhoCode.settings'
+ 
 from django.core.wsgi import get_wsgi_application
-
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'pythonweb.settings')
-
-application = get_wsgi_application()
+from django.contrib.staticfiles.handlers import StaticFilesHandler
+application = StaticFilesHandler(get_wsgi_application())
